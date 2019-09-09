ee404ab4e1f201be4fb49d5e00f424ac2195b9a0
David Kuseh <davegru203@gmail.com>
2019-09-09 14:12:18 +0000

    made some corrections

--- src/App.js
+++ src/App.js
@@ -7,12 +7,6 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [scoreHome, setScoreHome] = useState(0);
   const [scoreAway, setScoreAway] = useState(0);
-  const [quarter, setQuarter] = useState(1);
-
-  //Quarter
-  const currentQuarter = () => {
-    setQuarter(quarter + 1);
-  }
 
   //HomeTeam
   const fieldGoalHome = () => {
