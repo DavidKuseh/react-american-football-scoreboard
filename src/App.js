676bafa7c3c415e274adc64739c434da176ff783
David Kuseh <davegru203@gmail.com>
2019-09-09 11:54:31 +0000

    imported useState and set up score for home and away teams

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 //TODO: STEP 1 - Import the useState hook.
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [score, setScore] = useState(0);
+
+  //HomeTeam
+  const fieldGoal = () => {
+    setScore(score + 3);
+  }
+
+  const touchDown = () => {
+    setScore(score + 7);
+  }
+
+  //AwayTeam
+  const fieldGoal = () => {
+    setScore(score + 3);
+  }
+
+  const touchDown = () => {
+    setScore(score + 3);
+  }
 
   return (
     <div className="container">
